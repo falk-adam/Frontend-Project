5f9fde321109c1f23d2b5db4c3d5d6745793a86a
Teo Lind <teo.l.lind@gmail.com>
2025-04-23 20:36:51 +0200

    fixed import of icons (didnt work), made an example to test in HomePage component

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,9 +1,17 @@
 //HomePage, browse and search for listings
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAirbnb } from "@fortawesome/free-brands-svg-icons";
 
 function HomePage() {
   return (
     <div className="bg-gray-400 flex-grow flex p-10">
       <h2>Look at this HomePage</h2>
+      <FontAwesomeIcon
+        icon={faAirbnb}
+        style={{ color: "red" }}
+        transform={{ rotate: 180 }}
+        size="2x"
+      />
     </div>
   );
 }
