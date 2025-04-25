ef915ebe552a2ac721b3b010ffa887ec5f98f0f5
Teo Lind <teo.l.lind@gmail.com>
2025-04-25 09:40:12 +0200

    wrapped App return in AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,26 @@ import CreateListingPage from "./components/pages/CreateListingPage";
 import UserProfilePage from "./components/pages/UserProfilePage";
 import LoginPage from "./components/pages/LoginPage";
 import UserRegistrationPage from "./components/pages/UserRegistrationPage";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <main className="flex w-full min-h-[calc(100vh-8.5rem)] p-10 bg-gray-300 my-2">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/listing/:listingId" element={<ListingPage />} />
-          <Route path="/booking/:listingId" element={<CreateBookingPage />} />
-          <Route path="/user/:username" element={<UserProfilePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<UserRegistrationPage />} />
-          <Route path="/listing/create" element={<CreateListingPage />} />
-        </Routes>
-      </main>
-      <Footer />
+      <AuthProvider>
+        <Header />
+        <main className="flex w-full min-h-[calc(100vh-8.5rem)] p-10 bg-gray-300 my-2">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/listing/:listingId" element={<ListingPage />} />
+            <Route path="/booking/:listingId" element={<CreateBookingPage />} />
+            <Route path="/user" element={<UserProfilePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<UserRegistrationPage />} />
+            <Route path="/listing/create" element={<CreateListingPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </AuthProvider>
     </BrowserRouter>
   );
 }
