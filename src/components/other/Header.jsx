fe169dfdd86e195b1c33b6074bbabffd2cb68ece
Teo Lind <teo.l.lind@gmail.com>
2025-04-30 17:32:09 +0200

    fixed re-sizing of icons for mobile

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
           icon={faAirbnb}
           style={{ color: "red" }}
           transform={{ rotate: 180 }}
-          size="2x"
+          className="text-3xl"
         />
         <h1 className="text-red-500 font-bold text-[22px] tracking-normal leading-[16px] max-mobile:text-[14px] max-mobile:pl-1">
           airbnb clone
@@ -25,16 +25,20 @@ function Header() {
       </Link>
 
       {/*Right-side container: link to "request become a host page (not implemented) and a dropdown menu (not implemented, only icons currently*/}
-      <div className="h-full flex gap-3 items-center max-mobile:gap-2">
-        <div className="text-gray-600 p-3 text-center leading-[16px] max-mobile:p-1 max-mobile:w-15 max-mobile:text-[12px]">
+      <div className="h-full flex gap-2 items-center max-mobile:gap-1">
+        <div className="text-gray-600 p-3 text-center leading-[15px] max-mobile:p-1 max-mobile:w-15 max-mobile:text-[11px]">
           Become a host
         </div>
-        <div className="border-2 border-gray-300 rounded-4xl flex gap-3 p-2 pl-3 max-mobile:p-1 max-mobile:pl-2">
-          <FontAwesomeIcon icon={faBars} style={{ color: "gray" }} size="2x" />
+        <div className="border-2 border-gray-300 rounded-4xl items-center flex gap-3 p-1 pl-3 max-mobile:p-1 max-mobile:pl-2 max-mobile:gap-2">
+          <FontAwesomeIcon
+            icon={faBars}
+            style={{ color: "gray" }}
+            className="text-2xl max-mobile:text-[18px]"
+          />
           <FontAwesomeIcon
             icon={faCircleUser}
             style={{ color: "gray" }}
-            size="2x"
+            className="text-4xl max-mobile:text-[28px]"
           />
         </div>
       </div>
