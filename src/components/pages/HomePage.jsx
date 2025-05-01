ccb3a964c975a8cbe9d1b76adcb4474f97d388f8
Teo Lind <teo.l.lind@gmail.com>
2025-05-01 10:07:52 +0200

    made a connection to backend and to show very basic info for all listings on homepage

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,19 +1,64 @@
 //HomePage, browse and search for listings
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAirbnb } from "@fortawesome/free-brands-svg-icons";
+import { useState, useEffect } from "react";
+import { getAllListings } from "../../api/listingService";
 
-function HomePage() {
+const HomePage = () => {
+  const [listings, setListings] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchAllListings = async () => {
+      try {
+        const data = await getAllListings();
+        setListings(data);
+      } catch (error) {
+        console.log("Error: " + error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAllListings();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
   return (
-    <div className="bg-gray-400 flex-grow flex p-10">
-      <h2>Look at this HomePage</h2>
-      <FontAwesomeIcon
-        icon={faAirbnb}
-        style={{ color: "red" }}
-        transform={{ rotate: 180 }}
-        size="2x"
-      />
+    <div className="flex flex-wrap w-full h-full m-2 p-5 bg-gray-100">
+      {listings.map((listing) => (
+        <div key={listing.id} className="w-110 h-110 m-5 p-5 bg-gray-200">
+          <p className="leading-[28px]">
+            <h2 className="text-[22px] font-bold">{listing.title}</h2>
+            <strong>host:</strong> {listing.host.name}, <br />
+            <strong>price:</strong> {listing.pricePerNight}, <br />
+            <strong>capacity:</strong> {listing.capacity},<br />
+            <strong>location:</strong> {listing.location},<br />
+            <strong>available dates:</strong>
+            {listing.availableDates.map((availableDateRange) => (
+              <>
+                {" "}
+                {availableDateRange.startDate} to {availableDateRange.endDate},
+              </>
+            ))}
+            <br />
+            <strong>utilities:</strong>
+            {listing.utilities.map((element) => (
+              <> {element},</>
+            ))}
+            <br />
+            <strong>images:</strong>
+            {listing.imageUrls.map((element) => (
+              <> {element},</>
+            ))}
+            <br />
+            <strong>description:</strong> {listing.description},<br />
+            <strong>avg. rating:</strong> {listing.averageRating},<br />
+            <strong>posted:</strong> {listing.createdAt},<br />
+          </p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default HomePage;
