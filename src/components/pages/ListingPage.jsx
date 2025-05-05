80890e5c15cbaf88aa98b363fc898a711e1bbda8
falk-adam <falk47.adam@gmail.com>
2025-05-05 21:03:57 +0200

    created a grid for images

--- src/components/pages/ListingPage.jsx
+++ src/components/pages/ListingPage.jsx
@@ -44,20 +44,28 @@ function ListingPage() {
     <div className="flex flex-wrap h-full w-full ml-20 mr-20 mt-10 bg-gray-200">
         <div>
           <h1 className="font-bold text-[30px] bg-green-400 w-120 pb-3">
-            {listing.title}
+            {listing.title} 
           </h1> 
-          <p className="bg-gray-500 h-200 w-210">IMG PLACE HOLDER</p>
-          <p>
-            <strong>LOCATION: </strong> {listing.location} <br />
-            GUESTS: {listing.capacity} 
-          </p>
+          <p className="bg-gray-500 h-200 w-210 rounded-xl">IMG PLACE HOLDER</p>
+          <p className="text-[22px]"> {listing.location} </p>
+          <p>{listing.capacity} guests allowed</p>
           <h2 className="font-bold text-[20px]">Description</h2>
           <p className="">{listing.description}</p>
+          <h2>{listing.host.name}</h2>
+          <h3>{listing.averageRating} rating</h3>
+          <div className="flex max-w-md">
+            <div className="grid grid-cols-2 gap-4">
+              <div className="bg-gray-500 h-50 w-50 rounded-xl"></div>
+              <div className="bg-gray-500 h-50 w-50 rounded-xl"></div>
+              <div className="bg-gray-500 h-50 w-50 rounded-xl"></div>
+              <div className="bg-gray-500 h-50 w-50 rounded-xl"></div>
+            </div>
+          </div>
         </div>
 
     </div>
 
-    );
+    ); 
 
 }
   export default ListingPage;
