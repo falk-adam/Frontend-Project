f598d5450cf29d1d8b0d2edb717197ba506be6cd
Teo Lind <teo.l.lind@gmail.com>
2025-05-05 12:29:28 +0200

    refined menu options with a master list

--- src/components/header/HeaderMenu.jsx
+++ src/components/header/HeaderMenu.jsx
@@ -30,23 +30,23 @@ function HeaderMenu() {
     }
   }, [currentUser]);
 
+  //list classes, used for both options mappping and logout
+  const listClasses =
+    "w-full h-9 p-2 pl-4 flex items-center text-[15px] hover:bg-gray-100 max-mobile:justify-center max-mobile:pl-2 max-mobile:text-[12px] max-mobile:h-6";
+
   return (
     <nav className="bg-white absolute right-6 top-18 w-50 outline-solid outline-2 outline-gray-300 rounded-lg shadow-lg overflow-hidden max-mobile:right-0 max-mobile:w-screen max-mobile:rounded-none max-mobile:top-15">
       <ul>
         {/*map the options in the options array to a list*/}
         {options.map((option, index) => (
           <Link to={option.url} key={index}>
-            <li className="w-full h-8 p-2 pl-4 flex items-center text-[14px] hover:bg-gray-100 max-mobile:justify-center max-mobile:pl-2 max-mobile:text-[12px] max-mobile:h-6">
-              {option.text}
-            </li>
+            <li className={listClasses}>{option.text}</li>
           </Link>
         ))}
         {/*logout needs an onClick function (logout) and is therefor not in options array, but instead separated out here*/}
         {currentUser && (
           <Link to="/" onClick={handleLogout}>
-            <li className="w-full h-8 p-2 text-left text-[14px] hover:bg-gray-100">
-              Logout
-            </li>
+            <li className={listClasses}>Logout</li>
           </Link>
         )}
       </ul>
--- src/components/header/MenuOptionConstants.js
+++ src/components/header/MenuOptionConstants.js
@@ -3,35 +3,30 @@
  *
  * Hold the options for header menu depending on user role,
  * collected in separate file from HeaderMenu due to separation
- * of concerns
+ * of concerns and ease of access
  */
 
+//all possible menu options, local variable
+const MENU_ALL_OPTIONS = [
+  { text: "Login", url: "/login" },
+  { text: "Register", url: "/register" },
+  { text: "User profile", url: "/user" },
+  { text: "My Bookings", url: "/user/bookings" },
+  { text: "Create Listing", url: "/user/new_listing" },
+  { text: "Manage Listings", url: "/user/listings" },
+  { text: "Admin Dashboard", url: "/admin" },
+];
+
+//menu options per user type, exportable variable
 export const MENU_OPTIONS = {
-  MENU_DEFAULT: [
-    { text: "Login", url: "/login" },
-    { text: "Register", url: "/register" },
-  ],
+  MENU_DEFAULT: MENU_ALL_OPTIONS.slice(0, 2),
 
   //option for user with role USER
-  MENU_USER: [
-    { text: "User profile", url: "/user" },
-    { text: "My Bookings", url: "/" },
-  ],
+  MENU_USER: MENU_ALL_OPTIONS.slice(3, 5),
 
-  //additional options for user with role HOST
-  MENU_HOST: [
-    { text: "My Profile", url: "/user" },
-    { text: "My Bookings", url: "/" },
-    { text: "Create Listing", url: "/" },
-    { text: "Manage Listings", url: "/" },
-  ],
+  //options for user with role HOST
+  MENU_HOST: MENU_ALL_OPTIONS.slice(3, 7),
 
-  //additional options for user with role ADMIN
-  MENU_ADMIN: [
-    { text: "My Profile", url: "/user" },
-    { text: "My Bookings", url: "/" },
-    { text: "Create Listing", url: "/" },
-    { text: "Manage Listings", url: "/" },
-    { text: "Admin Dashboard", url: "/" },
-  ],
+  //options for user with role ADMIN
+  MENU_ADMIN: MENU_ALL_OPTIONS.slice(3, 8),
 };
