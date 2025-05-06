112caac69f10d7d1a6b53a7a3acbd24298be2d87
Teo Lind <teo.l.lind@gmail.com>
2025-05-06 10:29:41 +0200

    started styling homepage

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAirbnb } from "@fortawesome/free-brands-svg-icons";
 import { faCircleUser, faBars } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
 import HeaderMenu from "./HeaderMenu";
 import ToggleButton from "../other/ToggleButton";
 
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -34,40 +34,46 @@ const HomePage = () => {
   return (
     /*all styling in current return is temporary/subject for change, made just to test backend link,
      e.g., listings will be shown in listing card component instead*/
-    <div className="flex flex-wrap w-full h-full m-2 p-5 bg-gray-100">
-      {listings.map((listing) => (
-        <div key={listing.id} className="w-110 h-110 m-5 p-5 bg-gray-200">
-          <p className="leading-[28px]">
-            <h2 className="text-[22px] font-bold">{listing.title}</h2>
-            <strong>host:</strong> {listing.host.name}, <br />
-            <strong>price:</strong> {listing.pricePerNight}, <br />
-            <strong>capacity:</strong> {listing.capacity},<br />
-            <strong>location:</strong> {listing.location},<br />
-            <strong>available dates:</strong>
-            {listing.availableDates.map((availableDateRange) => (
-              <>
-                {" "}
-                {availableDateRange.startDate} to {availableDateRange.endDate},
-              </>
-            ))}
-            <br />
-            <strong>utilities:</strong>
-            {listing.utilities.map((element) => (
-              <> {element},</>
-            ))}
-            <br />
-            <strong>images:</strong>
-            {listing.imageUrls.map((element) => (
-              <> {element},</>
-            ))}
-            <br />
-            <strong>description:</strong> {listing.description},<br />
-            <strong>avg. rating:</strong> {listing.averageRating},<br />
-            <strong>posted:</strong> {listing.createdAt},<br />
-          </p>
-        </div>
-      ))}
-    </div>
+    <>
+      {/*searchbar component will be implemented here*/}
+      <div className="w-full h-full outline-solid outline-2 outline-gray-300 rounded-lg">
+        {/*filter/utilities buttons go here*/}
+        <div className="flex flex-wrap w-full bg-gray-300"></div>
+        {listings.map((listing) => (
+          <div key={listing.id} className="w-110 h-110 m-5 p-5 bg-gray-200">
+            <p className="leading-[28px]">
+              <h2 className="text-[22px] font-bold">{listing.title}</h2>
+              <strong>host:</strong> {listing.host.name}, <br />
+              <strong>price:</strong> {listing.pricePerNight}, <br />
+              <strong>capacity:</strong> {listing.capacity},<br />
+              <strong>location:</strong> {listing.location},<br />
+              <strong>available dates:</strong>
+              {listing.availableDates.map((availableDateRange) => (
+                <>
+                  {" "}
+                  {availableDateRange.startDate} to {availableDateRange.endDate}
+                  ,
+                </>
+              ))}
+              <br />
+              <strong>utilities:</strong>
+              {listing.utilities.map((element) => (
+                <> {element},</>
+              ))}
+              <br />
+              <strong>images:</strong>
+              {listing.imageUrls.map((element) => (
+                <> {element},</>
+              ))}
+              <br />
+              <strong>description:</strong> {listing.description},<br />
+              <strong>avg. rating:</strong> {listing.averageRating},<br />
+              <strong>posted:</strong> {listing.createdAt},<br />
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
