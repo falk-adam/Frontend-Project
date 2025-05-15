d86a864309c6c8795298e3d27431bd79a1295831
falk-adam <falk47.adam@gmail.com>
2025-05-15 12:23:54 +0200

    boxes for layout updated

--- src/components/pages/CreateBookingPage.jsx
+++ src/components/pages/CreateBookingPage.jsx
@@ -13,13 +13,14 @@ function CreateBookingPage() {
   
   return (
     <div className="w-full h-full flex flex-col bg-white">
-      <div className="bg-gray-400 w-full flex flex-col gap-2 justify-center items-center p-10">
+      <div className="w-full flex flex-col gap-2 justify-center items-center p-10">
         <div className="bg-amber-950 h-20 w-80 justify-center items-center">progress bar</div>
       </div>
       <div className="bg-green-800 flex flex-row h-dvh m-10 gap-10">
-        <div className="bg-blue-400 h-full w-full"></div>
-        <div className="flex flex-col bg-red-400 h-full w-full relative">
-          <div className="bg-yellow-600 h-25 w-full absolute bottom-0"></div>
+        <div className="bg-blue-400 h-full w-full">FORM</div>
+        <div className="bg-red-400 h-full w-10/17 relative flex flex-col">
+          <div className="bg-purple-900 w-full h-full mb-10">BOOKNIG CARD</div>
+          <div className="bg-yellow-600 h-25 w-full bottom-0">BUTTON</div>
         </div>
       </div>
     </div>
