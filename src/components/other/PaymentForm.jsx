d1a6b2e17af502ba88227dba09632236008e09c2
falk-adam <falk47.adam@gmail.com>
2025-05-19 10:01:13 +0200

    full layout complete for desktop

--- src/components/other/PaymentForm.jsx
+++ src/components/other/PaymentForm.jsx
@@ -2,12 +2,17 @@ import { useState } from "react";
 
 
 function PaymentForm() {
+    const [paymentMethod, setPaymentMethod] = useState("creditCard")
     const [paymentInfo, setPaymentInfo] = useState({
         cardNr: "",
         expDate: "",
         cvv: "",
         cardName: ""
     })
+
+    const handleMethodChange = (method) => {
+        setPaymentMethod(method)
+    };
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -19,12 +24,40 @@ function PaymentForm() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        console.log("Payment Method: ", paymentMethod)
         console.log("Payment Info: ", paymentInfo)   
     }
     
 
     return (
         <form onSubmit={handleSubmit} className="w-full mx-auto p-4">
+            <div>
+                <label className="block mb-1">Payment Method</label>
+
+                <div className={`flex items center p-4 border rounded-lg cursor pointer mb-2 ${
+                    paymentMethod === "creditCard" ? "border-red-400" : "border-gray-300"
+                }`}
+                onClick={() => handleMethodChange("creditCard")}
+                >
+                    <span className={`w-5 h-5 rounded-full border-4 mr-3 ${
+                        paymentMethod === "creditCard" ? "border-red-500" : "border-gray-300"
+                    }`} ></span>
+                    <span>Credit Card</span>
+                </div>
+
+                
+                <div className={`flex items center p-4 border rounded-lg cursor pointer mb-2 ${
+                    paymentMethod === "paypal" ? "border-red-400" : "border-gray-300"
+                }`}
+                onClick={() => handleMethodChange("paypal")}
+                >
+                    
+                    <span className={`w-5 h-5 rounded-full border-4 mr-3 ${
+                        paymentMethod === "paypal" ? "border-red-500" : "border-gray-300"
+                    }`} ></span>
+                    <span>PayPal</span>
+                </div>
+            </div>
             <div className="grid grid-cols-3 gap-4">
                 <div>
                     <label className="block mb-1" htmlFor="cardNr">
@@ -32,7 +65,8 @@ function PaymentForm() {
                     </label>
                     <input 
                     name="cardNr"
-                    className="border border-gray-300 w-[100%] rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-300 placeholder-gray-400"
+                    type="text"
+                    className="border border-gray-300 w-full rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-red-300 placeholder-gray-400"
                     placeholder="XXXX-XXXX-XXXX-XXXX"
                     value={paymentInfo.cardNr}
                     onChange={handleChange}
@@ -44,7 +78,8 @@ function PaymentForm() {
                     </label>
                     <input 
                         name="expDate"
-                        className="border border-gray-300 w-[50%] rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-300 placeholder-gray-400"
+                        type="text"
+                        className="border border-gray-300 w-full rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-red-300 placeholder-gray-400"
                         placeholder="XX / XX"
                         value={paymentInfo.expDate}
                         onChange={handleChange}
@@ -56,7 +91,8 @@ function PaymentForm() {
                     </label>
                     <input
                         name="cvv"
-                        className="border border-gray-300 w-[50%] rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-300 placeholder-gray-400"
+                        type="text"
+                        className="border border-gray-300 w-full rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-red-300 placeholder-gray-400"
                         placeholder="XXX"
                         value={paymentInfo.cvv}
                         onChange={handleChange}
@@ -64,13 +100,19 @@ function PaymentForm() {
                 </div>
             </div>
             
-            <input
-                name="cardName"
-                className="border border-gray-300 w-full rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-300 placeholder-gray-400"
-                placeholder="John Doe"
-                value={paymentInfo.cardName}
-                onChange={handleChange}
-            />
+            <div className="mt-5">
+                <label className="block mb-1" htmlFor="cardName">
+                    Name on Card
+                </label>
+                <input
+                    name="cardName"
+                    type="text"
+                    className="border border-gray-300 w-full rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-300 placeholder-gray-400"
+                    placeholder="John Doe"
+                    value={paymentInfo.cardName}
+                    onChange={handleChange}
+                />
+            </div>
         </form>
     )
 }
